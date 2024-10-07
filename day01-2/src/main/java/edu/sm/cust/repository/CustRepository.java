a995b734d246cffa1afd2a0cf5cd906db6072ed6
eunbum <bum17822@naver.com>
2024-10-07 11:12:00 +0900

    day01-2 최신화 1007

--- day01-2/src/main/java/edu/sm/cust/repository/CustRepository.java
+++ day01-2/src/main/java/edu/sm/cust/repository/CustRepository.java
@@ -2,7 +2,9 @@ package edu.sm.cust.repository;
 
 import edu.sm.dto.Cust;
 import edu.sm.frame.SMRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository //레파지토리 역할을 하는 컴퍼넌트임
 public class CustRepository implements SMRepository<String, Cust> {
     @Override
     public void insert(Cust cust) {
--- day01-2/src/main/java/edu/sm/cust/service/CustService.java
+++ day01-2/src/main/java/edu/sm/cust/service/CustService.java
@@ -3,9 +3,14 @@ package edu.sm.cust.service;
 import edu.sm.dto.Cust;
 import edu.sm.frame.SMRepository;
 import edu.sm.frame.SMService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
+@Service("cservice")    //서비스역할을 하는 컴퍼넌트임
 public class CustService implements SMService<String, Cust> {
 
+    @Autowired  //자동적으로 SMRepository 타입의 객체를 찾아서 객체를 주입해줌.
     SMRepository<String, Cust> repository;
 
     @Override
