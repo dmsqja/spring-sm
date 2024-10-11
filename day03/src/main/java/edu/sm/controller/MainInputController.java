64bcd900542529f71ba7d426414c11f36b7ec953
eunbum <bum17822@naver.com>
2024-10-11 11:36:11 +0900

    1011

--- day03/src/main/java/edu/sm/controller/MainInputController.java
+++ day03/src/main/java/edu/sm/controller/MainInputController.java
@@ -1,5 +1,6 @@
 package edu.sm.controller;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,21 +10,30 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @Slf4j
 public class MainInputController {
+    @RequestMapping("/logoutimpl")
+    public String logoutimpl(HttpSession session, Model model) {
+        if(session != null){
+            session.invalidate();
+        }
+        return "redirect:/";
+    }
+
+
     @RequestMapping("/loginimpl")
     public String loginimpl(Model model,
                             @RequestParam("id") String id,
-                            @RequestParam("pwd") String pwd) {
+                            @RequestParam("pwd") String pwd,
+                            HttpSession session) {
         log.info("ID:"+id);
         log.info("PWD:"+pwd);
-        return "index";
-    }
-
-    @RequestMapping("/signupimpl")
-    public String signupimpl(Model model,
-                             @RequestParam("id") String id,
-                             @RequestParam("pwd") String pwd) {
-        log.info("ID:"+id);
-        log.info("PWD:"+pwd);
-        return "index";
+        // aaa, 111
+        String next = "redirect:/";
+        if(id.equals("aaa") && pwd.equals("111")){
+            session.setAttribute("loginid", id);
+        }else{
+            model.addAttribute("center","loginfail");
+            next = "index";
+        }
+        return next;
     }
 }
