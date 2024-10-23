e18d91e56765175aefbd9a245f2553d2e1579592
eunbum <bum17822@naver.com>
2024-10-23 11:39:33 +0900

    Feat: Add Admin System

---
+++ day03_iot/src/main/java/edu/sm/Main.java
@@ -0,0 +1,30 @@
+package edu.sm;
+
+import edu.sm.util.HttpSendData;
+
+import java.util.Random;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String url = "http://127.0.0.1/iot/power";
+        String data = "100.0";
+        Random r = new Random();
+        for(int i=0;i<100;i++){
+            float num = r.nextFloat() * 100;
+            HttpSendData.send(url,num+"");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+
+
+
+
+
